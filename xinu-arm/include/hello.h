66baa0df6d7a169f536c42955e78a5f272917f17
Peter Zhang <yz79@indiana.edu>
2014-09-19 10:44:19 -0400

    all

---
+++ xinu-arm/include/hello.h
@@ -0,0 +1,16 @@
+/*
+ * @file hello.h
+ * @provides printHello function 
+ */
+ /* For Xinu OS for Assignment 1 of P536
+  * Peter Zhang (yz79)
+  * Eriya Terada (eterada)
+  *
+  * Sept 18, 2014
+  */
+
+
+/**
+ * prints hello...
+ */
+void printHello(char *s);
