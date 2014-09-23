af47d293f0e4221d752c26251fb0fac6fb4cc303
Eriya Terada <eterada@indiana.edu>
2014-09-23 19:25:21 -0400

    Added stuff

--- asst2-yz79-eterada/main.c
+++ asst2-yz79-eterada/main.c
@@ -11,6 +11,16 @@
 #include <curses.h>
 #include <sys/time.h>
 #include <unistd.h>
+#include <pthreads.h>
+#include "matmult.h"
+
+//
+// A struct shared between main and worker thread so that
+// worker thread can get orders from main thread.
+//
+matrix_mult_struct m_struct;
+
+
 
 /*
  * worker(char input) -> int
@@ -28,20 +38,21 @@ int worker(char input){
   // save its results to FILE and terminate.
   if (input == 'z' || input == 'Z'){
     printw("Calculate curent multiplication, save, and quit\n");
-    /* TO-DO: write to file */
-    endwin();
+    *m_struct.tofile = 1; // Set tofile flag in m_struct to 1
     return 0;
   }
   
   // If input is 's' or 'S', the program will stop multiplying matrices
   if (input == 's' || input == 'S'){
     printw("\nCalculation stopped.\n");
-    input = getch();
+    *m_struct.stop = 1; // set stop flag in m_struct to 1
+
   }
   
   // If input is 't' or 'T', the program will continue multiplying matrices
   if (input == 't' || input == 'T'){
     printw("\n");
+    *m_struct.stop = 0; // set stop flag in m_struct to 0, i.e. continue calculation
   }
   
   return 0;
@@ -73,6 +84,26 @@ void read_from_file(){
  *
  */
 int main(){
+  // Allocate memory for stuff in m_struct
+  m_struct.output = malloc(DIM*DIM*4)
+  m_struct.print_current_indices = malloc(sizeof(uint8_t));
+  m_struct.stop = malloc(sizeof(uint8_t));
+  m_struct.alive = malloc(sizeof(uint8_t));
+  m_struct.tofile = malloc(sizeof(uint8_t));
+  
+  // Initialize values in m_struct
+  *m_struct.print_current_indices = 0;
+  *m_struct.stop = 0;
+  *m_struct.alive = 1;
+  *m_struct.tofile = 0;
+  
+  // Fill 2 matrices with numbers
+  populate_matrix(matrix1, DIM*DIM);
+  populate_matrix(matrix2, DIM*DIM);
+  
+  // Create thread
+  pthread_t thread;
+  pthread_create(&thread, NULL, &matrix_mult, &m_struct);
   
   //first, initialize curses
   WINDOW * default_win=initscr();
@@ -86,6 +117,25 @@ int main(){
     worker(input);
   }while ((input = getch()) != 'q' && (input != 'Q'));
 
+  // Make sure that the alive flag in m_struct is set to false
+  // before exiting.
+  if (input == 'q' || input == 'Q'){
+    *m_struct.alive = 0;
+  }
+  
+  // Join threads
+  pthread_join(thread, NULL);
+  
+  // end curses window
   endwin();
+  
+  
+  // Free stuff!!!
+  free(m_struct.output);
+  free(m_struct.print_current_indices);
+  free(m_struct.stop);
+  free(m_struct.alive);
+  free(m_struct.tofile);
+  
   return 0;
 }
