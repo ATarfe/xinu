6ad736966ccc24775ee0e6eed1e676b7bcc75974
Eriya Terada <eterada@indiana.edu>
2014-10-01 20:09:44 -0400

    fixes

--- xinu-arm/include/prodcons.h
+++ xinu-arm/include/prodcons.h
@@ -2,11 +2,13 @@
 #include	<stdio.h>						
 #include  <semaphore.h> //include/semaphore.h
 /*Global	vaible	for	producer	consumer*/		
-extern	int n;		
+extern int n;
 //semaphores:
+/*
 semaphore sem_producer;
 semaphore sem_consumer;
 semaphore mutex;
+*/
 /*function	Prototype*/
 void consumer(int count);	
 void producer(int count);	
