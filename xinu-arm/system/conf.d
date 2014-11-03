b9e96d59d9c1c01dd069995f1469d26526834653
peteraldaron <peteraldaron@gmail.com>
2014-11-02 20:34:02 -0500

    init hw7

---
+++ xinu-arm/system/conf.d
@@ -0,0 +1,26 @@
+../system/conf.o: ../system/conf.c ../include/conf.h ../include/stddef.h \
+ ../include/device.h ../include/loopback.h ../include/semaphore.h \
+ ../include/queue.h ../include/kernel.h ../include/null.h \
+ ../include/uart.h ../include/tty.h ../include/stdarg.h
+
+../include/conf.h:
+
+../include/stddef.h:
+
+../include/device.h:
+
+../include/loopback.h:
+
+../include/semaphore.h:
+
+../include/queue.h:
+
+../include/kernel.h:
+
+../include/null.h:
+
+../include/uart.h:
+
+../include/tty.h:
+
+../include/stdarg.h:
