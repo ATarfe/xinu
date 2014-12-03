148ad42d3ae4c77204fbd110711246bc883167e2
peteraldaron <peteraldaron@gmail.com>
2014-12-02 21:11:32 -0500

    fs wip

--- exinu/system/fs.c
+++ exinu/system/fs.c
@@ -1,6 +1,10 @@
 #include <fs.h>
 #include <stdlib.h>
 #include <stdio.h>
+#include <kernel.h>
+#include <stddef.h>
+#include <string.h>
+#include <bufpool.h>
 
 
 int fopen(char *filename, int flags){
@@ -25,6 +29,22 @@ int fwrite(int fd, void *buf, int nbytes)
 /* filesystem functions */
 int mkfs(int dev, int num_inodes)
 {
+    //make bs dev:blocksize for default values:
+    if(OK==mkbsdev(
+                dev,
+                MDEV_BLOCK_SIZE,
+                num_inodes*ceil(((float)sizeof(inode)/(float)MDEV_BLOCK_SIZE)))
+            )
+    {
+        //init fsystem to block 0:
+        struct fsystem fs;
+        fs.nblocks=num_inodes*ceil(((float)sizeof(inode)/(float)MDEV_BLOCK_SIZE));
+        fs.blocksz=MDEV_BLOCK_SIZE;
+        fs.ninodes=num_inodes;
+        fs.inodes_used=0;
+        bwrite(dev,0,0,,sizeof(fsystem));
+    }
+    else return SYSERR;
 }
 int mount(int dev)
 {
